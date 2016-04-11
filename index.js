ff0fdd967a94d62c93ec0aa2bc0224dbee6b6c6d
Martinovsky <martin.degenne@gmail.com>
2016-04-11 23:21:16 +0200

    play with mongoose schema

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
 var express = require('express');
 var app = express();
-
+ 
 var bdd = require('./models/connector');
-
+ 
 var mongoose = require('mongoose');
-
-
+ 
+ 
 app.get('/', function (req, res) {
-    bdd.connect();
-    bdd.db().once('open', function() {
-        
-        bdd.close();
-    } );
-    
     res.send('Hello World');
 });
-
+ 
 var server = app.listen(8081, function () {
+ 
+    var host = server.address().address
+    var port = server.address().port
+ 
+    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("Let's creat a character")
+    
+    bdd.connect();
+    
+    bdd.db().once('open', function() {
+        
+        var schema = new mongoose.Schema({
+            name: String,
+            type: String,
+            owner_user: mongoose.Schema.ObjectId,
+            actual_fiche: mongoose.Schema.ObjectId
+        });
+        
+        var character = mongoose.model('character', schema);
 
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("Example app listening at http://%s:%s", host, port)
+        /*var Vladimir = new character({
+            name : "Vladimir Kedchenkov",
+            type : "Vampire"
+        });*/
+        
+        character.find(function (err, characters) {
+            if (err) return console.error(err);
+            
+            characters[0].name = "Vladou";
+            
+            characters[0].save(function (err) {
+            if (err) return console.error(err);
+                console.log("Vladmir Kendchenkov est save in base")
+                
+             bdd.close();
+            });
+            
+        });         
+    } ); 
 });
